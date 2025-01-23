d0235b3fa135eecb2763c86c507feb87764d4016
robertrgeraghty@gmail.com <rrg053@utulsa.edu>
2025-01-23 03:10:01 -0600

    basic splash and homepage

--- on-call-analytics/src/routes/+layout.svelte
+++ on-call-analytics/src/routes/+layout.svelte
@@ -1,6 +1,57 @@
 <script lang="ts">
+	import Menu from '~icons/lucide/menu'
 	import '../app.css';
 	let { children } = $props();
+	
+	let theme = $state()
+	let themelist = [
+			"business",
+			"black", 
+			"light", 
+			"dark", 
+			"cupcake", 
+			"forest", 
+			"halloween", 
+			"lofi", 
+			"pastel", 
+			"wireframe", 
+			"synthwave", 
+			"retro", 
+			"cyberpunk", 
+			"valentine", 
+			"dracula", 
+			"aqua", 
+			"black", 
+			"luxury", 
+			"night", 
+			"soda", 
+			"emerald", 
+			"corporate"
+		]
+	theme = "black"
+	
 </script>
+<div data-theme="{theme}">
+	<div class="navbar w-full flex justify-between h-16 border-t border-b fixed bg-inherit z-30">
+		<div class="w-full flex justify-evenly">
+			<button class="btn btn-ghost absolute left-0">
+				<Menu class="" style="font-size: xx-large;"/>
+			</button>
+			<a class="text-xl md:text-4xl font-altehaas text-center" href="/">Robert Geraghty</a>
+		</div>
+		<!-- <div class="dropdown">
+			<div class="">
+				<span class="hidden md:inline mr-1">Theme</span>
+				<select class="select select-bordered" bind:value={theme}>
+					{#each themelist as t}
+						<option value={t}>{t}</option>
+					{/each}
+				</select>
+			</div>
+		</div> -->
+	</div>
 
 {@render children()}
+
+</div>
+
--- on-call-analytics/tailwind.config.ts
+++ on-call-analytics/tailwind.config.ts
@@ -1,14 +1,29 @@
 import containerQueries from '@tailwindcss/container-queries';
 import forms from '@tailwindcss/forms';
 import typography from '@tailwindcss/typography';
+import daisyui from "daisyui"
+import AlteHaasGroteskBold from '$lib/fonts/alte_haas_grotesk/AlteHaasGroteskBold.ttf';
+import AlteHaasGroteskRegular from '$lib/fonts/alte_haas_grotesk/AlteHaasGroteskRegular.ttf';
 import type { Config } from 'tailwindcss';
 
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: {}
+		extend: {
+			fontFamily: {
+				sans: ['Inter', 'ui-sans-serif', 'system-ui'],
+				serif: ['Merriweather', 'ui-serif', 'Georgia'],
+				mono: ['Menlo', 'ui-monospace', 'Monaco'],
+				altehaas: ['AlteHaasGrotesk'],
+			},
+		}
 	},
 
-	plugins: [typography, forms, containerQueries]
+	plugins: [typography, forms, containerQueries, daisyui],
+	daisyui: {
+		themes: [
+			"black","light", "business", "luxury"
+		]
+	}
 } satisfies Config;
