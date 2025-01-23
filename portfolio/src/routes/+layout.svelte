47dd71cbd0aba7a9798da0a70da1b8060003509c
robertrgeraghty@gmail.com <rrg053@utulsa.edu>
2025-01-23 03:19:17 -0600

    hacky header pop-in fix

--- portfolio/src/routes/+layout.svelte
+++ portfolio/src/routes/+layout.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
 	import Menu from '~icons/lucide/menu'
 	import '../app.css';
+	import { on } from 'svelte/events';
+	import { onMount } from 'svelte';
 	let { children } = $props();
-	
+	let draw = $state(false)
+
 	let theme = $state()
 	let themelist = [
 			"business",
@@ -29,27 +32,38 @@
 			"corporate"
 		]
 	theme = "black"
-	
+	onMount(() => {
+		toggle()
+	})
+
+	async function toggle() {
+		await new Promise( resolve => setTimeout(resolve, 200))
+		draw = !draw
+	}
+
 </script>
+
 <div data-theme="{theme}">
-	<div class="navbar w-full flex justify-between h-16 border-t border-b fixed bg-inherit z-30">
-		<div class="w-full flex justify-evenly">
-			<button class="btn btn-ghost absolute left-0">
-				<Menu class="" style="font-size: xx-large;"/>
-			</button>
-			<a class="text-xl md:text-4xl font-altehaas text-center" href="/">Robert Geraghty</a>
-		</div>
-		<!-- <div class="dropdown">
-			<div class="">
-				<span class="hidden md:inline mr-1">Theme</span>
-				<select class="select select-bordered" bind:value={theme}>
-					{#each themelist as t}
-						<option value={t}>{t}</option>
-					{/each}
-				</select>
+	{#if draw}
+		<div class="navbar w-full flex justify-between h-16 border-t border-b fixed bg-inherit z-30">
+			<div class="w-full flex justify-evenly">
+				<button class="btn btn-ghost absolute left-0">
+					<Menu class="" style="font-size: xx-large;"/>
+				</button>
+				<a class="text-xl md:text-4xl font-altehaas text-center" href="/">Robert Geraghty</a>
 			</div>
-		</div> -->
-	</div>
+			<!-- <div class="dropdown">
+				<div class="">
+					<span class="hidden md:inline mr-1">Theme</span>
+					<select class="select select-bordered" bind:value={theme}>
+						{#each themelist as t}
+							<option value={t}>{t}</option>
+						{/each}
+					</select>
+				</div>
+			</div> -->
+		</div>
+	{/if}
 
 {@render children()}
 
